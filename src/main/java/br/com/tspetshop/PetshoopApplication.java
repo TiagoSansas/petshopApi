0aa82051c9939faa27f38950b2c41e93e588bbae
Tiago Souza <tiagoeduardosouza@gmail.com>
2024-11-21 08:39:05 -0300

    Start Project PetShop

---
+++ src/main/java/br/com/tspetshop/PetshoopApplication.java
@@ -0,0 +1,13 @@
+package br.com.tspetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tspetshop/PetshoopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tspetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
